b9d61d8aa1e8d8382cf0c31233ea3a9e3fd7509a
JinaboCV <jinabocv@gmail.com>
2025-03-28 23:50:05 +0100

    read of crud

--- ecom/views.py
+++ ecom/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
-    return render(request, 'ecom/index.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'ecom/index.html', context=context)
 

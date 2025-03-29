e904a38feb717056dda1bf148406c33dfd63ba63
JinaboCV <jinabocv@gmail.com>
2025-03-29 08:46:10 +0100

    pagination an search

--- ecom/views.py
+++ ecom/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 def index(request):
     products = Product.objects.all()
+    item_name = request.GET.get('item-name')
+    if item_name != '' and item_name is not None:
+        products = Product.objects.filter(title__icontains = item_name)
+    paginator = Paginator(products, 4)
+    page_naumber = request.GET.get('page')
+    products = paginator.get_page(page_naumber)
     context = {'products': products}
     return render(request, 'ecom/index.html', context=context)
 
